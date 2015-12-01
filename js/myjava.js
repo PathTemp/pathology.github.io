6e5f3a69cea57b5382047bb351fd9bce636980ca
DiffPath <mikemoravek@gmail.com>
2015-11-30 23:26:17 -0600

    Update myjava.js

--- js/myjava.js
+++ js/myjava.js
@@ -6,6 +6,8 @@ var input2 = document.getElementById("DSubclass").value;
 var input3 = document.getElementById("DOrgan").value;
 var input4 = document.getElementById("DDescription").value;
 var input5 = document.getElementById("DTags").value;
+result.textContent = 'Your username is: ' + input1 + input2;
+alert(input1,input2,input3,input4,input5);
 }
 
 var subButton = document.getElementById('Publish');
