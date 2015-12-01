8a4a9ae68877a11f2abb49861c4c0664fe5ed327
DiffPath <mikemoravek@gmail.com>
2015-11-30 21:30:31 -0600

    Create myjava.js

---
+++ js/myjava.js
@@ -0,0 +1,9 @@
+function submitDisease()
+{
+var input1 = document.getElementById("DName")
+var input2 = document.getElementById("DSubclass")
+var input3 = document.getElementById("DOrgan")
+var input4 = document.getElementById("DDescription")
+var input5 = document.getElementById("DTags");
+alert(input1,input2,input3,input4,input5);
+}
