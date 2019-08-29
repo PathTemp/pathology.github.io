d2b11887c8529ee833198c9b34c163ab3665cb74
DiffPath <mikemoravek@gmail.com>
2019-08-29 11:20:11 -0500

    Update heart_transplant.js

--- js/heart_transplant.js
+++ js/heart_transplant.js
@@ -188,7 +188,7 @@ function clearTemplate() {
 
 function openNav() {
   document.getElementById("mySidenav").style.width = "150px";
-  document.getElementById("mySidenav").style.borderRight = "2px solid #111";
+  document.getElementById("mySidenav").style.borderRight = "3px solid #111";
 }
 
 function closeNav() {
